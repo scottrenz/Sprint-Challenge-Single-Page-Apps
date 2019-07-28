3180430f7e220d6304f75db8b992a18603314097
Scott Renz <scottrenz@scottrenz.com>
2019-07-28 01:42:28 -0400

    Changed style of link tab buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
+import { Link } from 'react-router-dom'
+
+import styled from 'styled-components'
 
 import TabNav from './components/TabNav.js';
 import Header from './components/Header.js';
@@ -9,11 +12,46 @@ import LocationList from './components/LocationList';
 import EpisodesList from './components/EpisodesList';
 import WelcomePage from './components/WelcomePage';
 
+const Nav = styled.nav`
+width: 10%;
+display: flex;
+width: 100%;
+justify-content: space-around;
+align-items: center;
+padding: 1.0rem;
+height: 5.8rem;
+`
+const Button = styled.button`
+color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid dodgerblue;
+  border-radius: 3px;
+  display: block;
+
+  `;
+  
+
 export default function App() {
     
   return <main>
 <Header />
-<TabNav />
+<Nav>
+<Button>
+<Link className='movie-card' to={`/home`}>Home</Link>
+</Button>
+<Button>
+<Link className='movie-card' to={`/characters`}>Characters</Link>
+</Button>
+<Button>
+<Link className='movie-card' to={`/locations`}>Locations</Link>
+</Button>
+<Button>
+<Link className='movie-card' to={`/episodes`}>Episodes</Link>
+</Button>
+</Nav>
+ {/* <TabNav /> */}
 <AppRouter>
    <Router>
    <Route path='/episodes' component={EpisodesList} />
