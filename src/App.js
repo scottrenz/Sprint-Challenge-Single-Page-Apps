d2b2fd1ec52b369d94cc720e4bdb49e3a463a3f9
Scott Renz <scottrenz@scottrenz.com>
2019-07-28 02:18:27 -0400

    commented out unused import tabnav

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 import styled from 'styled-components'
 
-import TabNav from './components/TabNav.js';
+// import TabNav from './components/TabNav.js';
 import Header from './components/Header.js';
 import AppRouter from './components/AppRouter.js';
 import CharacterList from './components/CharacterList';
